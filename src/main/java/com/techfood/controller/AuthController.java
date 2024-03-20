7f35551536e0cccefbe75fc70b4b2bff15dce1af
ySertacc <ysertac96@gmail.com>
2024-03-20 16:17:19 +0300

    Security Config düzenleme 3.

--- src/main/java/com/techfood/controller/AuthController.java
+++ src/main/java/com/techfood/controller/AuthController.java
@@ -3,11 +3,9 @@ package com.techfood.controller;
 import com.techfood.dto.RegisterUser;
 import com.techfood.entity.User;
 import com.techfood.service.AuthenticationService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -16,6 +14,11 @@ public class AuthController {
 
     public AuthController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "wlecome";
     }
 
     @PostMapping("/register")
